feca94353754df79a6e2c356eab7fbe5df77e107
Clint <clinthiles1992@gmail.com>
2024-09-13 10:47:12 -0700

    the speed of light is unreachable

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -1,16 +1,36 @@
-import './Modal.scss';
+import PropTypes from "prop-types";
+import "./Modal.scss";
 
 function Modal({ isOpen, onClose, photo }) {
   if (!isOpen) return null;
 
+  const handleContentClick = (e) => {
+    e.stopPropagation();
+  };
+
   return (
     <div className="modal-overlay" onClick={onClose}>
-      <div className="modal-content" >
-        <button className="modal-close" onClick={onClose}>×</button>
+      <div className="modal-content" onClick={handleContentClick}>
+        <button className="modal-close" onClick={onClose}>
+          ×
+        </button>
         <img src={photo.urls.full} alt={`Photo by ${photo.user.name}`} />
       </div>
     </div>
   );
 }
+
+Modal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  photo: PropTypes.shape({
+    urls: PropTypes.shape({
+      full: PropTypes.string.isRequired,
+    }).isRequired,
+    user: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default Modal;
