5281ee57fd8e3aaff9dbbbd5e61a0fc7a6dc5a3e
Clint Hiles <clinthiles1992@gmail.com>
2024-09-14 18:12:53 -0700

    commiting unsaved routes file

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
-  get "photos/index"
-  get "photos/show"
-  get "photos/create"
-  resources :posts
-  resources :users
+  resources :users, only: %i[index show create]
+  resources :photos, only: %i[index show create]
+  resources :likes, only: [:create]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
