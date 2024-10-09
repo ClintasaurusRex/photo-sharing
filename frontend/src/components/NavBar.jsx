add783cb759694cce6c723249d415544b24f8607
Clint <clinthiles1992@gmail.com>
2024-10-09 08:37:31 -0700

    Search bar semi working

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,8 +1,27 @@
 import "./NavBar.scss";
-import useSearch from "../hooks/useSearch";
+import { useState } from "react";
+import photos from "../mock-data/photos";
 
 const NavBar = () => {
-  const { search, InputChange, handleSearch } = useSearch();
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const InputChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    const filteredList = photos.filter((photo) => {
+      return (
+        !search ||
+        photo.user.name.toLowerCase().includes(search.toLowerCase()) ||
+        photo.location.city.toLowerCase().includes(search.toLowerCase()) ||
+        photo.location.country.toLowerCase().includes(search.toLowerCase())
+      );
+    });
+    setSearchResults(filteredList);
+  };
 
   return (
     <nav className="navbar">
@@ -31,6 +50,19 @@ const NavBar = () => {
           onChange={InputChange}
         />
       </form>
+      {searchResults.length > 0 && (
+        <div className="search-results">
+          {searchResults.map((photo) => (
+            <div key={photo.id} className="search-result-item">
+              <img src={photo.url} alt={`Photo by ${photo.user.name}`} />
+              <p>
+                {photo.location.city}, {photo.location.country}
+              </p>
+              <p>{photo.user.name}</p>
+            </div>
+          ))}
+        </div>
+      )}
       <ul className="navbar-links">
         <li>
           <a href="/login">Login</a>
@@ -42,4 +74,5 @@ const NavBar = () => {
     </nav>
   );
 };
+
 export default NavBar;
--- frontend/src/hooks/useSearch.js
+++ frontend/src/hooks/useSearch.js
@@ -1,21 +1,31 @@
 import { useState } from "react";
+import photos from "../mock-data/photos";
 
 const useSearch = () => {
   const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
 
   const InputChange = (e) => {
     setSearch(e.target.value);
   };
 
-  const handleSearch = (e) => {
-    e.preventDefault();
-    console.log(search);
+  const handleSearch = () => {
+    const filteredList = photos.filter((photo) => {
+      return (
+        !search ||
+        photo.user.name.toLowerCase().includes(search.toLowerCase()) ||
+        photo.location.city.toLowerCase().includes(search.toLowerCase()) ||
+        photo.location.country.toLowerCase().includes(search.toLowerCase())
+      );
+    });
+    setSearchResults(filteredList);
   };
 
   return {
     search,
     InputChange,
     handleSearch,
+    searchResults,
   };
 };
 
